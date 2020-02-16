1a4376adceb54ff0a6710ed6f8f33e0adfab54ee
Владимир <vladimirkovtun@192.168.1.170.local.ipnet.ua>
2020-02-16 19:14:52 +0200

    Initial commit

---
+++ blogengine/blog/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render
+from django.views.generic import View
+from django.shortcuts import redirect
+from django.shortcuts import get_object_or_404
+
+from .models import Post, Tag
+from .utils import *
+from .forms import TagForm, PostForm
+
+
+def posts_list(request):
+    posts = Post.objects.all()
+    return render(request, 'blog/index.html', context={'posts': posts})
+
+
+class PostDetail(ObjectDetailMixin, View):
+    model = Post
+    template = 'blog/post_detail.html'
+
+
+class PostCreate(ObjectCreateMixin, View):
+    model_form = PostForm
+    template = 'blog/post_create_form.html'
+
+
+class TagDetail(ObjectDetailMixin, View):
+    model = Tag
+    template = 'blog/tag_detail.html'
+
+
+class TagCreate(ObjectCreateMixin, View):
+    model_form = TagForm
+    template = 'blog/tag_create.html'
+
+class TagUpdate(View):
+    def get(self, request, slug):
+        tag = Tag.objects.get(slug__iexact=slug)
+        bound_form = TagForm(insance=tag)
+        return render(request, 'blog/tag_update_form.html', context={'form': bound_form, 'tag': tag})
+
+
+def tags_list(request):
+    tags = Tag.objects.all()
+    return render(request, 'blog/tags_list.html', context={'tags': tags})
+
